5b30cc869530e51efbfa0a75288aed081cec024a
Konstantino <papalamus@yandex.ru>
2014-02-26 19:32:47 +0400

    Added smth 4 multiplayer

--- src/main/java/model/engine/GameField.java
+++ src/main/java/model/engine/GameField.java
@@ -6,48 +6,55 @@ import model.cards_templates.Card;
 import model.cards_templates.Creature;
 
 public class GameField {
-    Field field;
-    Hand hand;
-    Hand EnemyHand;
-    int[]idols;
-    int[]enemyIdols;
-    Deck deck;
+    private int turn = 0;
+    Field[] fieldSides;
+    Hand[] hands;
+    int[][]idols;
+    Deck[] decks;
+    static final int playersCount = 2;
 
     static final int rowNum = 5;
     static final int columnNum = 3;
     private static final int idolBaseHealth = 10;
     {
-        hand = new Hand();
-        field = new Field(rowNum,columnNum);
-        idols = initializeIdols();
-        enemyIdols = initializeIdols();
-        deck = Deck.getTestDeck();
-    }
+        hands = new Hand[playersCount];
+        hands[0] = new Hand();
+        hands[1] = new Hand();
 
+        decks= new Deck[playersCount];
+        decks[0] = new Deck();
+        decks[1] = new Deck();
+        //TODO make normal deck, this is test.
+        decks[0] = Deck.getTestDeck();
+        //~~~~
+        fieldSides = new Field[playersCount];
+        fieldSides[0] = new Field(rowNum,columnNum);
+        fieldSides[1] = new Field(rowNum,columnNum);
+        idols = new int[playersCount][];
+        for(int i = 0 ;i<playersCount;i++){
+            idols[i] = initializeIdols();
+        }
+
+    }
 
     public void playCreature(Creature cc, int row, int column){
         FieldCreature fc = new FieldCreature(cc,null);
-        if (field.getHex(row,column)==null) {
-            field.setHex(fc,row,column);
+        if (getMyField().getHex(row, column)==null) {
+            getMyField().setHex(fc, row, column);
         }
 
     }
 
 
     public boolean playCard(int CardNumber){
-       Card playCard = hand.getCard(CardNumber);
+       Card playCard = getMyHand().getCard(CardNumber);
         if (playCard instanceof Creature) {
-            Integer row =0 ,column= 0 ;
             int[] coords= new int[2];
             Programm.requestHex(coords);
             playCreature((Creature) playCard,coords[0],coords[1]);
             return true;
         }
         return false;
-    }
-
-    public void endTurn(){
-        hand.addCard(deck.getTopCard());
     }
 
     ///Начальная инициализация идолов здоровьем 10
@@ -60,45 +67,35 @@ public class GameField {
         return result;
     }
 
-    public int[] getEnemyIdols() {
-        return enemyIdols;
+    private Hand getMyHand(){
+        return hands[turn];
     }
 
-    public void setEnemyIdols(int[] enemyIdols) {
-        this.enemyIdols = enemyIdols;
-    }
-
-    public int[] getIdols() {
-        return idols;
-    }
-
-    public void setIdols(int[] idols) {
-        this.idols = idols;
-    }
-
-    public Hand getHand() {
-        return hand;
-    }
-
-    public void setHand(Hand hand) {
-        this.hand = hand;
+    private Field getMyField(){
+        return fieldSides[turn];
     }
 
     @Override
     public String toString() {
         StringBuffer sb = new StringBuffer();
-        for(int i=0;i<rowNum;i++){
-            sb.append("[ ");
-            for(int j=0;j<columnNum;j++) {
-                if (field.getHex(i,j) != null) {
-                    sb.append(field.getHex(i,j).getAttack()+ "/" +field.getHex(i,j).getHealth()+" ," );
-                } else {
-                    sb.append(" X ,");
+        for(int k = 0 ;k<playersCount;k++){
+            for(int i=0;i<rowNum;i++){
+                sb.append("[ ");
+                for(int j=0;j<columnNum;j++) {
+                    if (fieldSides[j].getHex(i,j) != null) {
+                        sb.append(fieldSides[k].getHex(i,j).getAttack()+ "/" +fieldSides[k].getHex(i,j).getHealth()+" ," );
+                    } else {
+                        sb.append(" X ,");
+                    }
                 }
+                sb.append(" ]");
+                sb.append("\n");
             }
-            sb.append(" ]");
-            sb.append("\n");
         }
         return sb.toString();
+    }
+
+    public void endTurn(){
+        turn = (turn==0)? 1 :0;
     }
 }
